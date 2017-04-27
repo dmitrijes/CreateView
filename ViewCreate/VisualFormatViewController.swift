c0394a8e51c4bfb2942c958ccd780b11c64b6f09
Dima <dzmitry_fralou@epbygrow0352.grodno.epam.com>
2017-04-27 14:26:31 +0300

    VFL

--- ViewCreate/VisualFormatViewController.swift
+++ ViewCreate/VisualFormatViewController.swift
@@ -10,26 +10,47 @@ import UIKit
 
 class VisualFormatViewController: UIViewController {
 
+    @IBOutlet weak var buttonCreate: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    private var count = 0
+    var allConstraintsForButton = [NSLayoutConstraint]()
+    var allConstraintsForView = [NSLayoutConstraint]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        
+        buttonCreate.translatesAutoresizingMaskIntoConstraints = false
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let buttonHorizontal = "[buttonCreate(200)]"
+        let buttonVertical = "V:|-20-[buttonCreate(50)]"
+        let views = ["buttonCreate" : buttonCreate!]
+        let buttonHorizontalConstrains = NSLayoutConstraint.constraints(withVisualFormat: buttonHorizontal, options: [], metrics: nil, views: views)
+        let buttonVerticalConstrains = NSLayoutConstraint.constraints(withVisualFormat: buttonVertical, options: [], metrics: nil, views: views)
+        allConstraintsForButton += buttonHorizontalConstrains
+        allConstraintsForButton += buttonVerticalConstrains
+        
+        NSLayoutConstraint.activate(allConstraintsForButton)
     }
-    */
+
+    @IBAction func createView(_ sender: UIButton) {
+        
+        let newView = UIView()
+        newView.backgroundColor = UIColor.red
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(newView)
+        let newViewHorizontal = "|-[newView(1000)]-|"
+        let newViewVertical = "|-[newView(1000)]-|"
+        let newViews = ["newView" : newView]
+        let newViewHorizontalConstrains = NSLayoutConstraint.constraints(withVisualFormat: newViewHorizontal, options: [], metrics: nil, views: newViews)
+        let newViewVerticalConstrains = NSLayoutConstraint.constraints(withVisualFormat: newViewVertical, options: [], metrics: nil, views: newViews)
+        allConstraintsForView += newViewHorizontalConstrains
+        allConstraintsForView += newViewVerticalConstrains
+        
+        NSLayoutConstraint.activate(allConstraintsForView)
+        
+        
+        count += 1
+    }
 
 }
